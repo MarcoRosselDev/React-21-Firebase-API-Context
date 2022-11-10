0ae32ae9ad37dcd523b9cc6693c33a3d3723ed67
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-10 06:26:41 -0300

    log changes on input pass and email

--- src/components/Register.js
+++ src/components/Register.js
@@ -8,16 +8,26 @@ export const Register = () => {
 
   //para actualizar el state email and password usamos =>
   const handleChange = (e) => {
-    console.log(e);
+    console.log(e.target.name, e.target.value);
   };
 
   return (
     <form>
       <label htmlFor="email">Email</label>
-      <input type="email" name="email" placeholder="youremail@company.ltd" />
+      <input
+        type="email"
+        name="email"
+        placeholder="youremail@company.ltd"
+        onChange={handleChange}
+      />
 
       <label htmlFor="password">Password</label>
-      <input type="password" name="password" id="password" />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        onChange={handleChange}
+      />
     </form>
   );
 };
