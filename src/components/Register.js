891ae381127674dc164c229309910c4a5c960ba2
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-10 06:18:55 -0300

    register funtionality

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,3 +1,23 @@
+import { useState } from "react";
+
 export const Register = () => {
-  return <div>Register</div>;
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  //para actualizar el state email and password usamos =>
+  const handleChange = (e) => {
+    console.log(e);
+  };
+
+  return (
+    <form>
+      <label htmlFor="email">Email</label>
+      <input type="email" name="email" placeholder="youremail@company.ltd" />
+
+      <label htmlFor="password">Password</label>
+      <input type="password" name="password" id="password" />
+    </form>
+  );
 };
