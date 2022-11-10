f46b357af9221e2ef89f7adf354b66be60d3aa84
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-10 01:53:06 -0300

    config firebase 2

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+const Home = () => {
+  const goo = () => {
+    console.log("jola");
+  };
+  return (
+    <div>
+      <div className="text-center align-items-center mt-9">
+        <div className="font-bold">
+          <h1>texto de prueba de h1</h1>
+          <button onClick={() => goo} className="btn-md bg-red-600 h-9">
+            testing button
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
