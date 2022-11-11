69f92c7a7073fed0e2883d86d41e91195b7a9a26
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-11 05:58:49 -0300

    protected route app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Alert } from "./components/Alert";
 import { Home } from "./components/Home";
 import { Register } from "./components/Register";
-import { ProjectedRoute } from "./components/ProjectedRoute";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 import { Login } from "./components/Login";
 import { AuthProvider } from "./context/authContext";
 
@@ -11,11 +11,17 @@ const App = () => {
     <div className="bg-slate-200 h-screen text-white flex text-black ">
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/alert" element={<Alert />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/projected-route" element={<ProjectedRoute />}></Route>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/alert" element={<Alert />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </AuthProvider>
     </div>
