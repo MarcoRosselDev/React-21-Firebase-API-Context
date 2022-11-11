8370da356bae1a9b878bd35045cc582e2126ba1c
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-11 07:42:24 -0300

    home component styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,9 +13,18 @@ export const Home = () => {
   }
 
   return (
-    <div>
-      <h1>welcome {user.email}</h1>
-      <button onClick={handleLogout}>logout</button>
+    <div className="w-full max-w-xs m-auto text-black">
+      <div className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4 ">
+        <h1 className="text-xl mb-4 text-center">
+          welcome {user.displayName || user.email}
+        </h1>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+          onClick={handleLogout}
+        >
+          logout
+        </button>
+      </div>
     </div>
   );
 };
