55291f2bd00ce0db003baa71ac6126a844f3c8d0
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-11 06:06:08 -0300

    protected route app.js successful

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,11 @@
 import { async } from "@firebase/util";
-import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 
 export const Home = () => {
   const { user, logout, loading } = useAuth();
-  const navigate = useNavigate();
 
   const handleLogout = async () => {
     await logout();
-    navigate("/login");
   };
 
   if (loading) {
